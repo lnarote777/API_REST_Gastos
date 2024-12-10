0f9801cb8fc235109bc74babbebbeec20144f41a
lnarote777 <lnarote777@iesrafaelalberti.es>
2024-12-10 17:08:11 +0100

    Initial commit

---
+++ src/main/kotlin/com/example/planificador/ApiRestSeguraApplication.kt
@@ -0,0 +1,11 @@
+package com.example.planificador
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiRestSeguraApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiRestSeguraApplication>(*args)
+}
